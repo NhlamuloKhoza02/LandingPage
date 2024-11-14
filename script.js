adf96103f706bb6b9b841e72351978fbef05e424
MuloKhoza <150336612+NhlamuloKhoza02@users.noreply.github.com>
2024-11-14 10:04:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function showSignupModal() {
+    // Example: Show a signup modal or display a form
+    alert('Get Started button clicked!');
+    // You could also display a modal, scroll to a form, or any other action
+}
